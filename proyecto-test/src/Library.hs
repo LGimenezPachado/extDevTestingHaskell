d4491bc8e3c81a097105765f1e29627bb34391c1
LGimenezPachado <57446285+LGimenezPachado@users.noreply.github.com>
2025-05-16 14:46:37 +0000

    map, any, all

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -2,6 +2,27 @@ module Library where
 import PdePreludat
 import Data.Foldable (maximumBy)
 
-f :: [Bool]
-f = map even [1, 2, 3, 4]
+--pruebas con map
+fDeMap1 :: [Bool]
+fDeMap1 = map even [1, 2, 3, 4]
+
+--pruebas con any
+fDeAny1 :: Bool
+fDeAny1 = any even [1, 2, 3, 4]
+
+alMenosUnPar :: [Number] -> Bool
+alMenosUnPar xs = any even xs
+
+--pruebas con all
+fDeAll1 :: Bool
+fDeAll1 = all even [1, 2, 3, 4]
+
+todosEnLista :: [Number] -> [Number] -> Bool
+todosEnLista xs ys = all (`elem` ys) xs
+
+--pruebas con filter
+
+
+
+
 
--- proyecto-test/src/Spec.hs
+++ proyecto-test/src/Spec.hs
@@ -5,6 +5,35 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "f" $ do
+  
+  describe "fDeMap1" $ do
     it "devuelve la paridad de los números del 1 al 4" $ do
-      f `shouldBe` [False, True, False, True]
+      fDeMap1 `shouldBe` [False, True, False, True]
+
+  describe "fDeAny1" $ do
+    it "devuelve True si al menos un número del 1 al 4 es par" $ do
+      fDeAny1 `shouldBe` True
+
+  describe "fDeAll1" $ do
+    it "devuelve False si no todos los números del 1 al 4 son pares" $ do
+      fDeAll1 `shouldBe` False
+
+  describe "alMenosUnPar" $ do
+    it "Devuelve True si hay al menos un número par" $ do
+      alMenosUnPar [1, 2, 3] `shouldBe` True
+
+    it "Devuelve False si no hay números pares" $ do
+      alMenosUnPar [1, 3, 5] `shouldBe` False
+
+    it "Devuelve False con lista vacía" $ do
+      alMenosUnPar [] `shouldBe` False
+
+  describe "todosEnLista" $ do
+    it "Devuelve True si todos los elementos están en la otra lista" $ do
+      todosEnLista [1, 2] [1, 2, 3] `shouldBe` True
+
+    it "Devuelve False si al menos uno no está en la otra lista" $ do
+      todosEnLista [1, 4] [1, 2, 3] `shouldBe` False
+
+    it "Devuelve True con lista vacía (todos sus elementos están)" $ do
+      todosEnLista [] [1, 2, 3] `shouldBe` True
